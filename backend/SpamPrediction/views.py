bf91aef323b8dbd0d0dbb044f7ff1e4cbcea681e
haaris <haarishuda090@gmail.com>
2024-07-14 02:32:31 +0530

    updated views.py

--- backend/SpamPrediction/views.py
+++ backend/SpamPrediction/views.py
@@ -11,15 +11,9 @@ class SMSPredictView(APIView):
         if serializer.is_valid():
             sms_text = serializer.validated_data['text']
             prediction = predict_sms(sms_text)
-            result = "This SMS is likely spam." if prediction == 1 else "This SMS is likely not spam."
+            if prediction == 1:
+                result = "This SMS is likely spam." 
+            else:
+                result="This SMS is likely not spam."
             return Response({"prediction": result}, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class SpamPredictionAPIView(APIView):  # Define SpamPredictionAPIView
-    def get(self, request):
-        # Implement logic for handling GET request
-        return Response({"message": "GET request handled"}, status=status.HTTP_200_OK)
-
-    def post(self, request):
-        # Implement logic for handling POST request
-        return Response({"message": "POST request handled"}, status=status.HTTP_200_OK)
