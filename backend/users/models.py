53f36bd4edc79bedf1fdfac0c4359cb4a5414f31
haaris <haarishuda090@gmail.com>
2024-07-13 17:34:20 +0530

    created superuser

--- backend/users/models.py
+++ backend/users/models.py
@@ -53,7 +53,7 @@ class User(AbstractBaseUser):
     objects = UserManager()
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["name","tc"]
+    REQUIRED_FIELDS = ["name","upi_id"]
 
     def __str__(self):
         return self.email
