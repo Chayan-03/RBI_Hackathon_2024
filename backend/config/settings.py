fed798a13494ea060258e0a7a708d57c84d1bdc9
haaris <haarishuda090@gmail.com>
2024-07-13 12:56:59 +0530

    vercel se haarkar railway ki firaak me

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -157,6 +157,9 @@ AUTH_USER_MODEL = 'users.User'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR,'static'),
+]
 
 # jwt settings 
 SIMPLE_JWT = {
