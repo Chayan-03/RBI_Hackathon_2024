694630f9f4c49b1fad9a4ca9711658cfd9ef3532
Shivoo29 <2004skj@gmail.com>
2024-07-16 14:38:45 +0530

    created for ML (Mommy Lovers)

--- backend/SpamPrediction/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import SpamDetectionModel, TransactionPatternDetectionModel
-# Register your models here.
-admin.site.register(SpamDetectionModel)
-admin.site.register(TransactionPatternDetectionModel)
--- backend/SpamPrediction/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('predict_spam/', views.SMSPredictView.as_view()),
-]
